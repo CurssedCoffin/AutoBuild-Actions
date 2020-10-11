566babc1a54047f51fe60934c027681f65ef1850
CurssedCoffin <49067137+CurssedCoffin@users.noreply.github.com>
2020-10-11 14:53:24 +0800

    Update diy-script.sh

--- Scripts/diy-script.sh
+++ Scripts/diy-script.sh
@@ -8,9 +8,11 @@ Author=CurssedCoffin
 
 Default_File=./package/lean/default-settings/files/zzz-default-settings
 Lede_Version=`egrep -o "R[0-9]+\.[0-9]+\.[0-9]+" $Default_File`
+Openwrt_Version="$Lede_Version-`date +%Y%m%d`"
+AutoUpdate_Version=`awk 'NR==6' ./package/base-files/files/bin/AutoUpdate.sh | awk -F'[="]+' '/Version/{print $2}'`
 Compile_Date=`date +'%Y/%m/%d'`
 Compile_Time=`date +'%Y-%m-%d %H:%M:%S'`
-TARGET_PROFILE=`grep '^CONFIG_TARGET.*DEVICE.*=y' .config | sed -r 's/.*DEVICE_(.*)=y/\1/' | awk 'NR==1'`
+TARGET_PROFILE=`egrep -o "CONFIG_TARGET.*DEVICE.*=y" .config | sed -r 's/.*DEVICE_(.*)=y/\1/'`
 }
 
 GET_TARGET_INFO() {
@@ -21,6 +23,7 @@ TARGET_SUBTARGET=`awk -F'[="]+' '/TARGET_SUBTARGET/{print $2}' .config`
 ExtraPackages() {
 [ -d ./package/lean/$2 ] && rm -rf ./package/lean/$2
 [ -d ./$2 ] && rm -rf ./$2
+Retry_Times=3
 while [ ! -f $2/Makefile ]
 do
 	echo "[$(date "+%H:%M:%S")] Checking out $2 from $3 ..."
@@ -31,15 +34,22 @@ do
 	fi
 	if [ -f $2/Makefile ] || [ -f $2/README* ];then
 		echo "[$(date "+%H:%M:%S")] Package $2 detected!"
-		if [ $2 == OpenClash ];then
-			mv $2/luci-app-openclash ./package/lean
-		else
-			mv $2 ./package/lean
-		fi
+		case $2 in
+		OpenClash)
+			mv -f ./$2/luci-app-openclash ./package/lean
+		;;
+		openwrt-OpenAppFilter)
+			mv -f ./$2 ./package/lean
+		;;
+		*)
+			mv -f ./$2 ./package/lean
+		esac
 		rm -rf ./$2 > /dev/null 2>&1
 		break
 	else
-		echo "[$(date "+%H:%M:%S")] Checkout failed,retry in 3s."
+		[ $Retry_Times -lt 1 ] && echo "[$(date "+%H:%M:%S")] Skip check out package $1 ..." && break
+		echo "[$(date "+%H:%M:%S")] [$Retry_Times]Checkout failed,retry in 3s ..."
+		Retry_Times=$(($Retry_Times - 1))
 		rm -rf ./$2 > /dev/null 2>&1
 		sleep 3
 	fi
@@ -48,14 +58,17 @@ done
 
 mv2() {
 if [ -f $GITHUB_WORKSPACE/Customize/$1 ];then
-	if [ ! -d ./$2 ];then
-		echo "[$(date "+%H:%M:%S")] Creating new folder $2 ..."
-		mkdir ./$2
-	fi
 	echo "[$(date "+%H:%M:%S")] Moving Customize/$1 to $2 ..."
-	mv -f $GITHUB_WORKSPACE/Customize/$1 ./$2/$1
+	[ ! -d ./$2 ] && mkdir -p ./$2
+	if [ -z $3 ];then
+		[ -f ./$2/$1 ] && rm -f ./$2/$1 > /dev/null 2>&1
+		mv -f $GITHUB_WORKSPACE/Customize/$1 ./$2/$1
+	else
+		[ -f ./$2/$1 ] && rm -f ./$2/$3 > /dev/null 2>&1
+		mv -f $GITHUB_WORKSPACE/Customize/$1 ./$2/$3
+	fi
 else
-	echo "[$(date "+%H:%M:%S")] No $1 file detected!"
+	echo "[$(date "+%H:%M:%S")] File [$1] is not detected!"
 fi
 }
 
@@ -66,31 +79,44 @@ sed -i "s/#src-git helloworld/src-git helloworld/g" feeds.conf.default
 mv2 mac80211.sh package/kernel/mac80211/files/lib/wifi
 mv2 system package/base-files/files/etc/config
 mv2 AutoUpdate.sh package/base-files/files/bin
+mv2 firewall.config package/network/config/firewall/files
+mv2 banner package/base-files/files/etc
+mv2 mt76.mk package/kernel/mt76 Makefile
 
+ExtraPackages git luci-app-autoupdate https://github.com/Hyy2001X main
 ExtraPackages git luci-theme-argon https://github.com/jerrykuku 18.06
-# ExtraPackages svn luci-app-adguardhome https://github.com/Lienol/openwrt/trunk/package/diy
+# ExtraPackages git luci-app-adguardhome https://github.com/Hyy2001X master
 # ExtraPackages svn luci-app-smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
 # ExtraPackages svn smartdns https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t
 ExtraPackages git OpenClash https://github.com/vernesong master
 ExtraPackages git luci-app-serverchan https://github.com/tty228 master
 ExtraPackages svn luci-app-socat https://github.com/xiaorouji/openwrt-package/trunk/lienol
+# ExtraPackages git openwrt-upx https://github.com/Hyy2001X master
+# ExtraPackages svn luci-app-mentohust https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw
+# ExtraPackages svn mentohust https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw
+# ExtraPackages svn luci-theme-opentomato https://github.com/kenzok8/openwrt-packages/trunk
+# ExtraPackages svn luci-theme-opentomcat https://github.com/kenzok8/openwrt-packages/trunk
+# ExtraPackages svn luci-app-adguardhome https://github.com/Lienol/openwrt/trunk/package/diy
+# ExtraPackages git luci-app-adguardhome https://github.com/rufengsuixing master
+# ExtraPackages git openwrt-OpenAppFilter https://github.com/Lienol master
 }
 
 Diy-Part2() {
+mv2 mwan3 package/feeds/packages/mwan3/files/etc/config
 echo "Author: $Author"
-echo "Current Openwrt version: $Lede_Version-`date +%Y%m%d`"
-echo "Current Device: $TARGET_PROFILE"
+echo "Openwrt Version: $Openwrt_Version"
+echo "AutoUpdate Version: $AutoUpdate_Version"
+echo "Device: $TARGET_PROFILE"
 sed -i "s?$Lede_Version?$Lede_Version Compiled by $Author [$Compile_Date]?g" $Default_File
-echo "$Author" > ./package/base-files/files/etc/openwrt_author
-echo "$Lede_Version-`date +%Y%m%d`" > ./package/base-files/files/etc/openwrt_date
-echo "$TARGET_PROFILE" > ./package/base-files/files/etc/openwrt_device
+echo "$Openwrt_Version" > ./package/base-files/files/etc/openwrt_info
+sed -i "s?Openwrt?Openwrt $Openwrt_Version / AutoUpdate $AutoUpdate_Version?g" ./package/base-files/files/etc/banner
 }
 
 Diy-Part3() {
 GET_TARGET_INFO
 Default_Firmware=openwrt-$TARGET_BOARD-$TARGET_SUBTARGET-$TARGET_PROFILE-squashfs-sysupgrade.bin
-AutoBuild_Firmware=AutoBuild-$TARGET_PROFILE-Lede-$Lede_Version`(date +-%Y%m%d.bin)`
-AutoBuild_Detail=AutoBuild-$TARGET_PROFILE-Lede-$Lede_Version`(date +-%Y%m%d.detail)`
+AutoBuild_Firmware=AutoBuild-$TARGET_PROFILE-Lede-${Openwrt_Version}.bin
+AutoBuild_Detail=AutoBuild-$TARGET_PROFILE-Lede-${Openwrt_Version}.detail
 mkdir -p ./bin/Firmware
 echo "[$(date "+%H:%M:%S")] Moving $Default_Firmware to /bin/Firmware/$AutoBuild_Firmware ..."
 mv ./bin/targets/$TARGET_BOARD/$TARGET_SUBTARGET/$Default_Firmware ./bin/Firmware/$AutoBuild_Firmware
